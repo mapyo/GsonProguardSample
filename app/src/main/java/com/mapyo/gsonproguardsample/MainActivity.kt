6db95462cd8a2942e0fac455b57e7472ba514d13
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2017-04-04 08:02:27 +0900

    Refact use Java Scalars Converter

--- app/src/main/java/com/mapyo/gsonproguardsample/MainActivity.kt
+++ app/src/main/java/com/mapyo/gsonproguardsample/MainActivity.kt
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -20,13 +22,14 @@ class MainActivity : AppCompatActivity() {
 
     var stringList = mutableListOf<String>()
     var repoList = mutableListOf<Repository>()
+    lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val listView = findViewById(R.id.list_view) as ListView
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1)
+        adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1)
         listView.adapter = adapter
         listView.setOnItemClickListener { parent, view, position, id ->
             Toast.makeText(this, repoList[position].html_url, Toast.LENGTH_SHORT).show()
@@ -34,7 +37,7 @@ class MainActivity : AppCompatActivity() {
 
         val retrofit = Retrofit.Builder()
                 .baseUrl("https://api.github.com")
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(ScalarsConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
 
@@ -44,14 +47,29 @@ class MainActivity : AppCompatActivity() {
         retrofit.create(GitHubApi::class.java).fetchRepos(user, sort, direction)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ repositories ->
-                    repoList.addAll(repositories)
+                .subscribe({ jsonString ->
+                    setUpRepoList(jsonString)
 
-                    repositories.map {
-                        stringList.add("${it.id} ${it.name}")
-                    }
-                    adapter.addAll(stringList)
                 }, Throwable::printStackTrace)
+    }
+
+    private fun setUpRepoList(jsonString: String) {
+        val repositories = parse(jsonString)
+
+        repoList.addAll(repositories)
+
+        repositories.map {
+            stringList.add("${it.id} ${it.name}")
+        }
+
+        adapter.addAll(stringList)
+    }
+
+    private fun parse(jsonString: String): List<Repository> {
+        val listType = object : TypeToken<List<Repository>>() {}.type
+        val repositories = Gson().fromJson<List<Repository>>(jsonString, listType)
+
+        return repositories
     }
 
     interface GitHubApi {
@@ -60,6 +78,6 @@ class MainActivity : AppCompatActivity() {
                 @Path("user") user: String,
                 @Query("sort") sort: String,
                 @Query("direction") direction: String
-        ): Observable<List<Repository>>
+        ): Observable<String>
     }
 }
