42eb6e6095ce89c79fa0ec39a540f3a770d20575
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2017-04-16 08:42:02 +0900

    Use java

---
+++ app/src/main/java/com/mapyo/gsonproguardsample/Repository.java
@@ -0,0 +1,11 @@
+package com.mapyo.gsonproguardsample;
+
+
+public class Repository {
+    // @SerializedName("id")
+    int id;
+    // @SerializedName("name")
+    String name;
+    // @SerializedName("html_url")
+    String html_url;
+}
